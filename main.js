f39a57eed7a40cadbfd343de2fcc8b4cc63bcb3e
ALAYA1803 <rodrigoalaya_2004@hotmail.com>
2026-07-02 10:09:31 -0500

    fix:link of deployment backend

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 
 /* ── REGISTRATION MODAL ────────────────────────── */
 
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'https://mineguard-webservice.onrender.com/';
 
 let _currentLangData = {};
 
